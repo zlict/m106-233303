050815abad72a28065d0dad2fccbe722a51ccd87
Robin Bühler <git+r@obin.ch>
2023-03-27 07:41:22 +0000

    chore: add known statements

---
+++ examples/block-1/known-statements.sql
@@ -0,0 +1,10 @@
+SELECT -- Daten abfragen auch mit JOIN
+DELETE -- Daten löschen
+UPDATE -- Daten verändern
+INSERT -- Daten hinzufügen
+CREATE -- Datenbankelemente erstellen
+DROP -- Datenbankelemente löschen
+ALTER -- Datenbankelemente verändern
+
+
+BEGIN -- Block mit COMMIT, ROLLBACK, END, ...
